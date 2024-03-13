e418a7df2eb2235f554b5b1fac844c170ded18bf
Only-Czesio <marek@kamyk.me>
2024-03-13 09:21:23 +0100

    deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Marek_Kamyk_Personal_Website',
+  base: "https://only-czesio.github.io/Marek_Kamyk_Personal_Website/",
 })
